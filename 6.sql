8b922a5d58ee597d4acd1861b86a3e4fbf6c3eb7
keshavsgowda <keshavs9719@gmail.com>
2017-12-26 06:28:38 +0530

    Create 6.sql

---
+++ 6.sql
@@ -0,0 +1,15 @@
+SET SERVEROUTPUT ON
+CREATE OR REPLACE PROCEDURE findFactorial
+AS
+n number;
+fac number:=1;
+i number;
+BEGIN
+n:=&n;
+FOR i IN 1..n
+LOOP
+fac:= fac * i ;
+END LOOP;
+dbms_output.put_line('Factorial='||fac);
+END;
+/
