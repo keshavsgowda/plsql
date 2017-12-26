6434185fc56b9c8397fb53f9a2a08986285ea6c4
keshavsgowda <keshavs9719@gmail.com>
2017-12-26 06:23:01 +0530

    Create 2.sql

---
+++ 2.sql
@@ -0,0 +1,11 @@
+SET SERVEROUTPUT ON
+BEGIN
+UPDATE EMPLOYEE_DETAIL
+SET SALARY = CASE
+ WHEN DEPTNO = 10 THEN salary+(salary * 0.15)
+ ELSE salary  
+ END
+WHERE DEPTNO IN (10);
+dbms_output.put_line (TO_Char(SQL%ROWCOUNT)||'rowsaffected.');
+END;
+/
